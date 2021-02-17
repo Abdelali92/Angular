8b1fab3b080162417fe11e45ca55c107aa9a949c
Abdelali92 <64874853+Abdelali92@users.noreply.github.com>
2021-02-17 11:53:42 +0100

    Add files via upload

---
+++ single-todo.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TodoService } from '../services/todo.service';
+
+@Component({
+  selector: 'app-single-todo',
+  templateUrl: './single-todo.component.html',
+  styleUrls: ['./single-todo.component.css']
+})
+export class SingleTodoComponent implements OnInit {
+
+  todo;
+  erreur;
+
+  constructor(private route : ActivatedRoute, private todoservice : TodoService) { }
+
+  ngOnInit(): void {
+
+    const id = this.route.snapshot.params['id'];
+    this.todo = this.todoservice.getTodo(+id);
+
+    if(!this.todo){
+      this.erreur = "Id incorrect";
+    }
+  }
+
+}
